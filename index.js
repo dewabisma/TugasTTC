852fc95bc84b6419ede31d218d277f0cb164cd1c
unknown <idewagedebisma@gmail.com>
2020-10-16 16:38:09 +0800

    complete form and fetch API functions

--- index.js
+++ index.js
@@ -3,6 +3,54 @@ const form = document.querySelector(".form");
 const formName = document.querySelector("#name");
 const formEmail = document.querySelector("#email");
 const formMessage = document.querySelector("#message");
+const barMenu = document.querySelector(".bars-menu i");
+const modal = document.querySelector(".hovering-menu")
+const cardWrapper = document.querySelector(".wrap-card");
+
+window.onload(() => {
+    fetchApi();
+})
+
+const fetchApi = () => {
+    fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=5`).
+    then(response => response.json()).
+    then(data => {
+        data.forEach(val => {
+            const postWrap = document.createElement('div');
+            const postTitle = document.createElement('h2');
+
+            postTitle.innerHTML = `<a href="detail.html?id=${val.id}&type=posts">ID: ${val.id}| ${val.title} </a>`;
+
+            postWrap.appendChild(postTitle);
+
+            wrapper.appendChild(postWrap);
+        });
+    });
+}
+
+// function
+const createCard = (name,email) => {
+    const div = document.createElement('div');
+    const icon = document.createElement('i');
+    const heading = document.createElement('h3')
+    const text = document.createElement('p');
+    const button = document.createElement('button');
+
+    div.classList.add('card-item');
+    icon.classList.add('fas');
+    icon.classList.add('fa-user');
+    button.classList.add('button');
+    heading.textContent = name;
+    text.textContent = email;
+    button.textContent = "Learn More";
+    
+    div.appendChild(icon);
+    div.appendChild(heading);
+    div.appendChild(text);
+    div.appendChild(button);
+
+    return div;
+}
 
 // Events
 form.addEventListener('submit', event => {
@@ -30,4 +78,8 @@ form.addEventListener('submit', event => {
         })
         .catch(error => console.log(error));
     }
+})
+
+barMenu.addEventListener('click', event => {
+    modal.style.display = modal.style.display === "flex"? "none":"flex";
 })
